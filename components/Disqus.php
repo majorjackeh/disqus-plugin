46c7e334a9496101d83132e3b0e306980154b39b
Jack Collison <jack@mazemedia.co.uk>
2016-05-06 18:43:10 +0100

    Added options for JS callback and removed legacy disqus code and replaced to new disqus config function.

--- components/Disqus.php
+++ components/Disqus.php
@@ -21,8 +21,32 @@ class Disqus extends ComponentBase
     }
 
     public function onRun(){
+        // Core
         $this->page['disqusSite'] = DisqusSettingsModel::get('disqussite'); 
         $this->page['disqusProtocol'] = DisqusSettingsModel::get('disqusprotocol'); 
         $this->page['disqusDeveloperMode'] = DisqusSettingsModel::get('disqusdevmode'); 
+        
+        // Javascript functions
+        $this->page['preData'] = (!empty(DisqusSettingsModel::get('preData'))) 
+                                ? DisqusSettingsModel::get('preData') : null; 
+        $this->page['preInit'] = (!empty(DisqusSettingsModel::get('preInit'))) 
+                                ? DisqusSettingsModel::get('preInit') : null; 
+        $this->page['onInit'] = (!empty(DisqusSettingsModel::get('onInit'))) 
+                                ? DisqusSettingsModel::get('onInit') : null;
+        $this->page['afterRender'] = (!empty(DisqusSettingsModel::get('afterRender'))) 
+                                ? DisqusSettingsModel::get('afterRender') : null; 
+        $this->page['onReady'] = (!empty(DisqusSettingsModel::get('onReady'))) 
+                                ? DisqusSettingsModel::get('onReady') : null; 
+        $this->page['onNewComment'] = (!empty(DisqusSettingsModel::get('onNewComment'))) 
+                                ? DisqusSettingsModel::get('onNewComment') : null;
+        $this->page['preReset'] = (!empty(DisqusSettingsModel::get('preReset'))) 
+                                ? DisqusSettingsModel::get('preReset') : null; 
+        $this->page['onPaginate'] = (!empty(DisqusSettingsModel::get('onPaginate'))) 
+                                ? DisqusSettingsModel::get('onPaginate') : null; 
+        $this->page['onIdentify'] = (!empty(DisqusSettingsModel::get('onIdentify'))) 
+                                ? DisqusSettingsModel::get('onIdentify') : null;
+        $this->page['beforeComment'] = (!empty(DisqusSettingsModel::get('beforeComment'))) 
+                                ? DisqusSettingsModel::get('beforeComment') : null;
+        
     }
 }
